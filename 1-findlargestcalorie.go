00d225b78697ea40220e4b6c9f73c292cc671fe6
25Ericcheong <ericcheongkaikit@gmail.com>
2023-06-24 17:18:29 +1000

    instead of reading entire file, had to read file - line by line and append to array

--- 1-findlargestcalorie.go
+++ 1-findlargestcalorie.go
@@ -1,20 +1,33 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io/ioutil"
+	"log"
+	"os"
 )
 
 func main() {
 	fmt.Println("Advent of Code Day 1")
 	fmt.Println("Attempting to read provided file now")
 	fmt.Println("")
-	b, err := ioutil.ReadFile("./1-puzzle/adventofcode.com_2022_day_1_input.txt")
+	file, err := os.Open("./1-puzzle/adventofcode.com_2022_day_1_input.txt")
 
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("Error found when trying to open file: %s", err)
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
 	}
 
-	str := string(b)
-	fmt.Println(str)
+	if scanner.Err() != nil {
+		log.Fatalf("Error found while reading lines from file: %s", scanner.Err())
+	}
+
+	fmt.Println("Lines should now be in array as shown below")
+	fmt.Println(lines)
 }

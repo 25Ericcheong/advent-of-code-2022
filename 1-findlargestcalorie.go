dbcdc1e72fd0ea545f70541807a43f0c6083fece
25Ericcheong <ericcheongkaikit@gmail.com>
2023-06-24 18:13:14 +1000

    logic to find largest calorie value

--- 1-findlargestcalorie.go
+++ 1-findlargestcalorie.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 )
 
 const filePath = "./1-puzzle/adventofcode.com_2022_day_1_input.txt"
@@ -35,11 +36,31 @@ func main() {
 
 	fmt.Println("Each line found in find should now be in an array")
 
-	// var maxCalorie int
+	var maxCalorie int
+	var sum int
 	for index, data := range lines {
+		fmt.Println(sum)
 		fmt.Println(data)
-		if index == 20 {
+		if index == 30 {
 			break
 		}
+
+		if data == "" {
+			if sum > maxCalorie {
+				maxCalorie = sum
+			}
+			sum = 0
+
+		} else {
+
+			num, err := strconv.Atoi(data)
+			if err != nil {
+				log.Fatalf("Error found when trying to parse string to int: %s", err)
+			}
+
+			sum += num
+		}
 	}
+
+	fmt.Println("Each line found in find should now be in an array")
 }

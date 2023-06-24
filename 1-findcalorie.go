0ea8f6788da6a651026ccf35e37ec44911006e4c
25Ericcheong <ericcheongkaikit@gmail.com>
2023-06-24 18:34:59 +1000

    rename file and segregate function for getting/scanning data from file

--- 1-findcalorie.go
+++ 1-findcalorie.go
@@ -13,25 +13,17 @@ const filePath = "./1-puzzle/adventofcode.com_2022_day_1_input.txt"
 func main() {
 	fmt.Println("Advent of Code Day 1")
 	fmt.Println("Attempting to open file now")
-	file, err := os.Open(filePath)
+	file, errFile := os.Open(filePath)
 
-	if err != nil {
-		fmt.Printf("Error found when trying to open file: %s", err)
+	if errFile != nil {
+		log.Fatalf("Error found when trying to open file: %s", errFile)
 	}
 	defer file.Close()
 
-	var lines []string
-	scanner := bufio.NewScanner(file)
+	var lines, errScan = getData(file)
 
-	// note that this is similar to a while loop
-	// loop continues as long as .Scan() is true
-	for scanner.Scan() {
-		// calling Scan, generates next line of data found in file
-		lines = append(lines, scanner.Text())
-	}
-
-	if scanner.Err() != nil {
-		log.Fatalf("Error found while reading lines from file: %s", scanner.Err())
+	if errScan != nil {
+		log.Fatalf("Error while reading through each line of file: %s", errScan)
 	}
 
 	fmt.Println("Each line found in find should now be in an array")
@@ -57,4 +49,23 @@ func main() {
 	}
 
 	fmt.Printf("Program has finished execution. Largest calorie is found to be %d \n", maxCalorie)
+}
+
+func getData(file *os.File) ([]string, error) {
+	var lines []string
+	scanner := bufio.NewScanner(file)
+
+	// note that this is similar to a while loop
+	// loop continues as long as .Scan() is true
+	for scanner.Scan() {
+		// calling Scan, generates next line of data found in file
+		lines = append(lines, scanner.Text())
+	}
+
+	if scanner.Err() != nil {
+		return lines, scanner.Err()
+	}
+
+	fmt.Println("Each line found in find should now be in an array")
+	return lines, nil
 }

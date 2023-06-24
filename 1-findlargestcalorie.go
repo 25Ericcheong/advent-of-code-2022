c7aab71ae86e075695d72806a00a404b308b72fa
25Ericcheong <ericcheongkaikit@gmail.com>
2023-06-24 17:57:27 +1000

    now have loaded data into array data structure for logic implementation

--- 1-findlargestcalorie.go
+++ 1-findlargestcalorie.go
@@ -7,11 +7,12 @@ import (
 	"os"
 )
 
+const filePath = "./1-puzzle/adventofcode.com_2022_day_1_input.txt"
+
 func main() {
 	fmt.Println("Advent of Code Day 1")
-	fmt.Println("Attempting to read provided file now")
-	fmt.Println("")
-	file, err := os.Open("./1-puzzle/adventofcode.com_2022_day_1_input.txt")
+	fmt.Println("Attempting to open file now")
+	file, err := os.Open(filePath)
 
 	if err != nil {
 		fmt.Printf("Error found when trying to open file: %s", err)
@@ -20,7 +21,11 @@ func main() {
 
 	var lines []string
 	scanner := bufio.NewScanner(file)
+
+	// note that this is similar to a while loop
+	// loop continues as long as .Scan() is true
 	for scanner.Scan() {
+		// calling Scan, generates next line of data found in file
 		lines = append(lines, scanner.Text())
 	}
 
@@ -28,6 +33,13 @@ func main() {
 		log.Fatalf("Error found while reading lines from file: %s", scanner.Err())
 	}
 
-	fmt.Println("Lines should now be in array as shown below")
-	fmt.Println(lines)
+	fmt.Println("Each line found in find should now be in an array")
+
+	// var maxCalorie int
+	for index, data := range lines {
+		fmt.Println(data)
+		if index == 20 {
+			break
+		}
+	}
 }

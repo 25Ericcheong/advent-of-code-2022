bcea42f9e14b9b3e67526699e5e7e108fa0f8f2b
25Ericcheong <ericcheongkaikit@gmail.com>
2024-06-17 15:22:25 +0800

    feat(a_day_one): Able to get digit based on requirements for each line from input file

--- a_day_one/trebuchet.go
+++ a_day_one/trebuchet.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 )
 
 func Trebuchet() {
@@ -12,12 +13,14 @@ func Trebuchet() {
 	file, err := os.Open("./a_day_one/trebuchet_input.txt")
 
 	if err != nil {
-		log.Fatal("error when trying to open trebuchet input: ", err)
+		log.Fatal("error when trying to open trebuchet input file: ", err)
 	}
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		fmt.Println(scanner.Text())
+		firstDigit, lastDigit := getDigit(scanner.Text())
+		fmt.Printf("Total %d\n", firstDigit+lastDigit)
 	}
 
 	defer func(file *os.File) {
@@ -26,4 +29,28 @@ func Trebuchet() {
 			log.Fatal("error when trying to close file: ", err)
 		}
 	}(file)
+}
+
+func getDigit(text string) (firstDigit int, secondDigit int) {
+	textLength := len(text)
+	first := 0
+	second := 0
+
+	for i := 0; i < textLength; i++ {
+		num, err := strconv.Atoi(text[i : i+1])
+		if err == nil {
+			first = num
+			break
+		}
+	}
+
+	for i := textLength - 1; i > -1; i-- {
+		num, err := strconv.Atoi(text[i : i+1])
+
+		if err == nil {
+			second = num
+			break
+		}
+	}
+	return first * 10, second
 }
